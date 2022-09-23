89927691e2bdc3ddfd17d18addaba7b96fabbb75
DKondrov <9177778286@mail.ru>
2022-09-23 13:50:39 +0300

    Initial Commit

---
+++ ContactListApp/ContactListAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContactListAppApp.swift
+//  ContactListApp
+//
+//  Created by D KONDROV on 23.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct ContactListAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
